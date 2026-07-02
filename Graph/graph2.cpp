6d6a49f92c71ad180a60f72d5a41f041ea606bef
Aditya05-spec <adityapratapsinghthb@gmail.com>
2026-07-02 14:46:25 +0530

    Shortest path using DFS

--- Graph/graph2.cpp
+++ Graph/graph2.cpp
@@ -22,7 +22,20 @@ class Graph {
         }
     }
 
-    void shortestPath(int src , int dest) {
+    // Topological Sort using DFS
+    void topologicalSortDFS(int src , unordered_map<int , bool> &visited , stack<int> &ans) {  //Kahn's algorithm
+            visited[src] = true;
+            for(auto i : adjList[src]) {
+                if(!visited[i.first]) {
+                    topologicalSortDFS(i.first , visited , ans);
+                }
+            }
+            // While returning store the node in the stack
+            ans.push(src);
+        }
+
+    // Shortest Path using BFS
+    void shortestPathBFS(int src , int dest) {
         queue<int> q;
         unordered_map<int , bool> visited;
         unordered_map<int , int> parent;
@@ -57,29 +70,68 @@ class Graph {
         }
     }
 
+    // Shortest Path using DFS
+    void shortestPathDFS(int dest , stack<int> &topoOrder , int n) {
+        vector<int> dist(n , INT_MAX);
+        int src = topoOrder.top();
+        topoOrder.pop();
+        dist[src] = 0;
+
+        for(auto i : adjList[0]) {
+            if(dist[0] + i.second < dist[i.first]) {
+                dist[i.first] = dist[0] + i.second;
+            }
+        }
+
+        while(!topoOrder.empty()) {
+            int topElement = topoOrder.top();
+            topoOrder.pop();
+
+            if(dist[topElement] != INT_MAX) {
+                for(auto i : adjList[topElement]) {
+                    if(dist[topElement] + i.second < dist[i.first]) {
+                        dist[i.first] = dist[topElement] + i.second;
+                    }
+                }
+            }
+            else {
+
+            }
+        }
+        
+        cout << "Printing Ans : " << endl;
+        for(int i=0 ; i<n ; i++) {
+            cout << i << "-> " << dist[i] << endl;
+        }
+    }
+
 };
 
 
 
 int main() {
     Graph g;
-    g.addEdges(0,1,1,1);
-    g.addEdges(1,2,1,1);
-    g.addEdges(2,3,1,1);
+    g.addEdges(0,1,5,1);
+    g.addEdges(0,2,3,1);
+    g.addEdges(2,1,2,1);
 
-    g.addEdges(3,4,1,1);
-    g.addEdges(0,5,1,1);
-    g.addEdges(5,4,1,1);
+    g.addEdges(1,3,3,1);
+    g.addEdges(2,3,5,1);
+    g.addEdges(2,4,6,1); 
 
-    g.addEdges(0,6,1,1);
-    g.addEdges(6,7,1,1);
-    g.addEdges(7,8,1,1);
-    g.addEdges(8,4,1,1);
+    g.addEdges(4,3,1,0);
 
     g.printList();
 
-    int src = 0;
-    int dest = 4;
-    g.shortestPath(src , dest);
+    // int src = 0;
+    // int dest = 4;
+    // g.shortestPathBFS(src , dest);
+
+
+    stack<int> topoOrder;
+    unordered_map<int , bool> visited;
+    g.topologicalSortDFS(0 , visited , topoOrder);
+    g.shortestPathDFS(3 , topoOrder , 5);
+
     return 0;
 }
